ec02bc05dd2a811abeb0160b6ebf289414bc4372
EthanPS23 <ethanps23@gmail.com>
2019-02-11 21:54:29 -0700

    Added auto clicker program

---
+++ Clicker2/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Clicker2
+{
+    partial class frmClicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDelay = new System.Windows.Forms.TextBox();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.txtInterval = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.tmrInterval = new System.Windows.Forms.Timer(this.components);
+            this.tmrTime = new System.Windows.Forms.Timer(this.components);
+            this.tmrDelay = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Delay (s)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Time to run (min)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Click Interval (ms)";
+            // 
+            // txtDelay
+            // 
+            this.txtDelay.Location = new System.Drawing.Point(108, 6);
+            this.txtDelay.Name = "txtDelay";
+            this.txtDelay.Size = new System.Drawing.Size(100, 20);
+            this.txtDelay.TabIndex = 1;
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(108, 32);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(100, 20);
+            this.txtTime.TabIndex = 2;
+            // 
+            // txtInterval
+            // 
+            this.txtInterval.Location = new System.Drawing.Point(108, 58);
+            this.txtInterval.Name = "txtInterval";
+            this.txtInterval.Size = new System.Drawing.Size(100, 20);
+            this.txtInterval.TabIndex = 3;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(15, 87);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "&Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(119, 87);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(75, 23);
+            this.btnEnd.TabIndex = 5;
+            this.btnEnd.Text = "&End";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // tmrInterval
+            // 
+            this.tmrInterval.Tick += new System.EventHandler(this.tmrInterval_Tick);
+            // 
+            // tmrTime
+            // 
+            this.tmrTime.Interval = 60000;
+            this.tmrTime.Tick += new System.EventHandler(this.tmrTime_Tick);
+            // 
+            // tmrDelay
+            // 
+            //this.tmrDelay.Tick += new System.EventHandler(this.tmrDelay_Tick);
+            // 
+            // frmClicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(225, 119);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtInterval);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.txtDelay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmClicker";
+            this.ShowIcon = false;
+            this.Text = "Clicker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtInterval;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.TextBox txtDelay;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Timer tmrInterval;
+        private System.Windows.Forms.Timer tmrTime;
+        private System.Windows.Forms.Timer tmrDelay;
+    }
+}
+
---
+++ Clicker2/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Clicker2
+{
+    // Ethan Shipley
+    // December 7, 2018
+    // Simple auto clicker for user
+    // program does not contain catches for input verification
+    // created for fun use case of the timer
+    // use at own risk
+    // Inputs:
+    // Accepts a timed delay before starting
+    // how long the program is to run for
+    // interval time between clicks
+    public partial class frmClicker : Form
+    {
+        // set the mouse variables, counters and limits
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+        int counterTime = 0;
+        int timeRun;
+        int counterDelay = 0;
+        int delayTime;
+
+        public frmClicker()
+        {
+            InitializeComponent();
+        }
+
+        // perform the mouse click on the existing mouse location
+        public void DoMouseClick()
+        {
+            //Call the imported function with the cursor's current position
+            uint X = (uint)Cursor.Position.X;
+            uint Y = (uint)Cursor.Position.Y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+        }
+
+        // starts the clicker upon start button click
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int tmr;
+            // gets the length of time program is to run for
+            timeRun = Convert.ToInt32(txtTime.Text);
+            // gets the delay before the program is to begin clicking
+            delayTime = Convert.ToInt32(txtDelay.Text);
+            //  gets the interval between left button clicks
+            tmrInterval.Interval = Convert.ToInt32(txtInterval.Text);
+            // delays the program based on the delay limit and enables the timers
+            Thread.Sleep(delayTime * 1000);
+            tmrTime.Enabled = true;
+            tmrInterval.Enabled = true;
+            
+            tmr = tmrInterval.Interval;
+            
+        }
+
+        // upon each interval timer tick perform the mouse click
+        private void tmrInterval_Tick(object sender, EventArgs e)
+        {
+            tmrInterval.Interval = Convert.ToInt32(txtInterval.Text);
+            DoMouseClick();
+        }
+
+        // upon Time timer tick increase the Time counter. When counterTime equals timer run the program stops clicking
+        private void tmrTime_Tick(object sender, EventArgs e)
+        {
+            counterTime++;
+            // if counterTime equals timeRun then clicking is stopped 
+            if (counterTime == timeRun)
+            {
+                counterTime = 0;
+                tmrInterval.Enabled = false;
+            }
+        }
+
+        //// upon Delay timer tick the counter delay is increased and when it equals delay time it is then reset to zero
+        //private void tmrDelay_Tick(object sender, EventArgs e)
+        //{
+        //    counterDelay++;
+        //    if (counterDelay == delayTime)
+        //    {
+        //        counterDelay = 0;
+        //    }
+        //}
+
+        // ends the program from running when clicked
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            tmrInterval.Enabled = false;
+            tmrTime.Enabled = false;
+            tmrTime.Enabled = false;
+        }
+    }
+}
